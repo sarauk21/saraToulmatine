92e5be0d1a4213d6db7e23099f08f236aa23e8eb
saraToulmatine <78386379+sarauk21@users.noreply.github.com>
2022-01-03 20:09:09 +0000

    Add files via upload

--- project1/libs/js/script.js
+++ project1/libs/js/script.js
@@ -9,20 +9,32 @@ var map = L.map('map').setView([0, 0], 2);
     zoomOffset: -1,
    }).addTo(map);
 
-
   //var marker = L.marker([51.5, -0.09]).addTo(map);
 
+  function createDate(dt, type) {
+    var day = new Date(dt * 1000);
+    if (type == "long") {
+        let options = {
+            weekday: "long",
+            year: "numeric",
+            month: "long",
+            day: "numeric",
+        };
+        return day.toLocaleString("en-us", options); // Friday, January 15, 2021
+    } else {
+        return day.toLocaleString("en-us", { weekday: "long" }); // Friday
+    }
+}
 
-  //       ***************   Drop down  list of country   ***********
-
-   //    ********                      *******************
 $(document).ready(function () {
-
+    var border;
+    var lat;
+    var lng;
    //       ***************   pick up current location   ***********
 
    navigator.geolocation.getCurrentPosition(position => {
-    let lat = position.coords.latitude;
-    let lng = position.coords.longitude;
+    lat = position.coords.latitude;
+    lng = position.coords.longitude;
     console.log(lat);console.log(lng);
     var marker = L.marker([lat, lng]).addTo(map);
     //alert("87877777777777777777999999999999999999999999");
@@ -38,6 +50,7 @@ $(document).ready(function () {
             if (result.status.name == "ok") {
                 //alert(" getReversGeo stat ok js"); 
             $('#txtName').html(result['data']['countryName']);
+           
             codeCountry = result['data']['countryCode'];
             //alert(codeCountry);
             console.log(codeCountry);
@@ -50,8 +63,10 @@ $(document).ready(function () {
                 success: result => {
                    //console.log(JSON.stringify(result));
                    //console.log(" i am in geonam f");
-                   if (result['status']['name'] == "ok") {
-        
+                   if (result['status']['name'] == "ok") {   
+                       $('#countryName').val(result['data'][0]['countryName']);
+
+                       $('#txtNameHeader').html(result['data'][0]['countryName']);
                        $('#txtName').html(result['data'][0]['countryName']);
                        $('#txtContinent').html(result['data'][0]['continent']);
                        $('#txtCapital').html(result['data'][0]['capital']);
@@ -65,7 +80,56 @@ $(document).ready(function () {
                 }
            })
 
-                 // draw border on map 
+             //  get more information
+            $.ajax({
+               url: 'libs/php/getCountryMoreInfo.php',
+               type: 'POST',
+               dataType: 'json',
+               data: {"country": codeCountry,
+               "lang": 'en'},
+               success: result4 => {
+                  if (result4['status']['name'] == "ok") {
+                       $('#txtCallCode').html(result4['data'][0]['callingCodes']);
+                       var curr = "<br>" + result4['data'][0]['currencies'][0]['name'] + "<br>" + result4['data'][0]['currencies'][0]['code'] +
+                           "<br>" + result4['data'][0]['currencies'][0]['symbol'] ;
+                       $('#currency').html(curr);
+                         var text = "<br>";
+                       for (let i = 0; i < result4['data'][0]['borders'].length; i++) {
+                       text += result4['data'][0]['borders'][i] + "<br>";
+                       $('#txtBorders').html(text);
+                          }
+                    $('#flag').attr("src", result4['data'][0]['flags']['png']);
+
+                       }
+                      }
+           })
+      
+       //  wiki info for current location
+       var countryName1 = $("#countryName").val();
+       console.log("********  hiden country name");
+       console.log(countryName1);
+       console.log("********  hiden country name");
+       $.ajax({
+        url: "libs/php/wiki.php",
+        type: 'GET',
+        dataType: 'json',
+        data: {
+          place: countryName1
+        }, 
+        success: function(result) {                  
+           console.log('wiki info', result);      
+             if (result.status.name == "ok") {
+               $('#txtWikiImg').html('<img src=' + result.data.thumbnail.source +'><br>');
+               $('#txtWiki').html('Wikipedia: ' + result.data.extract +'<br>');
+             } 
+          },
+            error: function(jqXHR, textStatus, errorThrown) {
+                console.log(textStatus, errorThrown);
+                      }
+      })
+       
+    // draw border on map 
+ 
            $.ajax({
                url: 'libs/php/drawCountryBorder.php',
                type: 'POST',
@@ -80,11 +144,14 @@ $(document).ready(function () {
                           "weight": 5,
                           "opacity": 0.65
                         };
-            
-                       L.geoJSON(result2['data'], {
-                       style: myStyle
-                       }).addTo(map);
-               
+
+                        if (border) {
+                            border.remove()
+                           
+                        }
+                        border = L.geoJSON(result2['data'], {
+                            style: myStyle
+                            }).addTo(map);
                     }
                 }
            })
@@ -93,12 +160,9 @@ $(document).ready(function () {
 
         }
     })
-     // get country info
-     
+     // get country info    
 
 }) //end navigator
-
-
 
       //       ***************   Drop down  list of country   ***********
 $.ajax({
@@ -138,17 +202,51 @@ $('#sel').change(function () {
            //console.log(JSON.stringify(result));
            //console.log(" i am in geonam f");
            if (result['status']['name'] == "ok") {
-
+               $('#txtNameHeader').html(result['data'][0]['countryName']);
                $('#txtName').html(result['data'][0]['countryName']);
                $('#txtContinent').html(result['data'][0]['continent']);
                $('#txtCapital').html(result['data'][0]['capital']);
                $('#txtLanguages').html(result['data'][0]['languages']);
                $('#txtPopulation').html(result['data'][0]['population']);
                $('#txtArea').html(result['data'][0]['areaInSqKm']);
-               $('#currency').html(result['data'][0]['currencyCode'])
+              
            }
         }
    })
+
+   //  get more information
+   $.ajax({
+    url: 'libs/php/getCountryMoreInfo.php',
+    type: 'POST',
+    dataType: 'json',
+    data: {"country": $('#sel').val(),
+    "lang": 'en'},
+    success: result4 => {
+       //console.log(JSON.stringify(result));
+      
+       if (result4['status']['name'] == "ok") {
+           //alert(" more info");
+           //console.log(" more info insid if ");
+         
+        $('#txtCallCode').html(result4['data'][0]['callingCodes']);
+        $('#lat').html(result4['data'][0]['latlng'][0]); lat = result4['data'][0]['latlng'][0];
+        $('#long').html(result4['data'][0]['latlng'][1]); lng = result4['data'][0]['latlng'][1];
+
+        var curr = "<br>" + result4['data'][0]['currencies'][0]['name'] + "<br>" + result4['data'][0]['currencies'][0]['code'] +
+        "<br>" + result4['data'][0]['currencies'][0]['symbol'] ;
+        $('#currency').html(curr);
+        var text = "<br>";
+        for (let i = 0; i < result4['data'][0]['borders'].length; i++) {
+          text += result4['data'][0]['borders'][i] + "<br>";
+            $('#txtBorders').html(text);
+        }
+        $( '#flag').empty();
+        $('#flag').attr("src", result4['data'][0]['flags']['png']);
+
+       }
+    }
+})        
+
     // draw border on map 
     $.ajax({
         url: 'libs/php/drawCountryBorder.php',
@@ -165,15 +263,174 @@ $('#sel').change(function () {
                 "weight": 5,
                 "opacity": 0.65
             };
-            
-            L.geoJSON(result2['data'], {
-                style: myStyle
-            }).addTo(map);
-               
+            //console.log(border);
+            if(border){
+
+                border.remove();
+                border = L.geoJSON(result2['data'], {
+                    style: myStyle
+                   }).addTo(map);
+                
+            } 
            }
         }
    })
+
 });
+
+//********************************************************* */
+//      Weather
+//********************************************************* */
+$('#myBtnWeather').click(function() {
+  
+    $.ajax({
+        url: 'libs/php/getWeather.php',
+        type: 'POST',
+        dataType: 'json',
+        data: {"lat":  $('#lat').text(),
+        "lon":  $('#long').text()},
+        success: result7 => {
+           //console.log(JSON.stringify(result));
+           console.log(" i am in weather ajax");console.log( $('#lat').text());console.log( $('#lon').text());
+           console.log("bbbbb");console.log(lat);console.log(lng);
+           if (result7['status']['name'] == "ok") {
+    
+                 //Getting the min and max values for each day
+                for(i = 0; i<7; i++){
+            
+                    $('#day'+ (i+1) + 'Min').html( Number(result7['data'].list[i].main.temp_min - 273.15).toFixed(1)+ '°');
+                    $('#day'+ (i+1) + 'Max').html( Number(result7['data'].list[i].main.temp_max - 273.15).toFixed(1)+ '°');
+            
+                    $('#img'+ (i+1) ).attr("src", "http://openweathermap.org/img/wn/"+ result7['data'].list[i].weather[0].icon + '.png');
+                    $('#day'+ (i+1) + 'Des').html( result7['data'].list[i].weather[0].main +'<br>'
+                     + result7['data'].list[i].weather[0].description);
+                    
+                     $('#day'+ (i+1)).html(createDate(result7['data'].list[i].dt, "long") );
+                     
+                     //var day = new Date(result7['data'].list[i].dt*1000);
+                     //$('#day'+ (i+1)).html(day.toDateString());  // 'Fri Jan 15 2021'
+    
+                } 
+           }
+        }
+    })
+})
+
+//*********************************************************** */
+
+//********************************************************* */
+//      Wikipedia
+//********************************************************* */
+$('#myBtnWiki').click(function() {
+    console.log("wwwwwwwiiiiiiiiiiiiiiikkkkiiiiiiiiiiii");
+    var name2 = $('#txtName').text();
+    console.log(name2);
+    $.ajax({
+        url: "libs/php/wiki.php",
+        type: 'GET',
+        dataType: 'json',
+        data: {
+          place: $('#txtName').text()
+        }, 
+        success: function(result) {                  
+           console.log('wiki info', result);      
+             if (result.status.name == "ok") {
+               $('#txtWikiImg').html('<img src=' + result.data.thumbnail.source +'><br>');
+               $('#txtWiki').html('Wikipedia: ' + result.data.extract +'<br>');
+             } 
+          },
+            error: function(jqXHR, textStatus, errorThrown) {
+                console.log(textStatus, errorThrown);
+                      }
+                  });
+})
+
+//******Click country information button
+// Get the modal
+var modal = document.getElementById("myModal");
+
+// Get the button that opens the modal
+var btn = document.getElementById("myBtn");
+
+// Get the <span> element that closes the modal
+var span = document.getElementsByClassName("close")[0];
+
+// When the user clicks the button, open the modal 
+btn.onclick = function() {
+  modal.style.display = "block";
+}
+
+// When the user clicks on <span> (x), close the modal
+span.onclick = function() {
+  modal.style.display = "none";
+}
+
+// When the user clicks anywhere outside of the modal, close it
+window.onclick = function(event) {
+  if (event.target == modal) {
+    modal.style.display = "none";
+  }
+}
+
+//  ***   END country information button
+
+
+//******Click country Weather button
+// Get the modal
+var modalWeather = document.getElementById("myModalWeather");
+
+// Get the button that opens the modal
+var btnWeather = document.getElementById("myBtnWeather");
+
+// Get the <span> element that closes the modal
+var spanWeather = document.getElementsByClassName("closeWeather")[0];
+
+// When the user clicks the button, open the modal 
+btnWeather.onclick = function() {
+  modalWeather.style.display = "block";
+}
+
+// When the user clicks on <span> (x), close the modal
+spanWeather.onclick = function() {
+  modalWeather.style.display = "none";
+}
+
+// When the user clicks anywhere outside of the modal, close it
+window.onclick = function(event) {
+  if (event.target == modalWeather) {
+    modalWeather.style.display = "none";
+  }
+}
+
+//  ***   END country Weather button
+
+//******Click country Wikipedia button
+
+// Get the modal
+var modalWiki = document.getElementById("myModalWiki");
+
+// Get the button that opens the modal
+var btnWiki = document.getElementById("myBtnWiki");
+
+// Get the <span> element that closes the modal
+var spanWiki = document.getElementsByClassName("closeWiki")[0];
+
+// When the user clicks the button, open the modal 
+btnWiki.onclick = function() {
+  modalWiki.style.display = "block";
+}
+
+// When the user clicks on <span> (x), close the modal
+spanWiki.onclick = function() {
+  modalWiki.style.display = "none";
+}
+
+// When the user clicks anywhere outside of the modal, close it
+window.onclick = function(event) {
+  if (event.target == modalWiki) {
+    modalwiki.style.display = "none";
+  }
+}
  
 }); // end of $(document).ready(function ()/
 
