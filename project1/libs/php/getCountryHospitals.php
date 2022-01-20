918fe7dacabb3eb26f7ac32507318c4d2eef0b9c
saraToulmatine <78386379+sarauk21@users.noreply.github.com>
2022-01-20 22:37:33 +0000

    Add files via upload

--- project1/libs/php/getCountryHospitals.php
+++ project1/libs/php/getCountryHospitals.php
@@ -1,20 +1,41 @@
 <?php
-
 	// remove for production
 //die("in country info  mmmmmmmmmmmmm");
-	ini_set('display_errors', 'On');
-	error_reporting(E_ALL);
+//die($_REQUEST['country']);
 
-	$executionStartTime = microtime(true);
+ini_set('display_errors', 'On');
+error_reporting(E_ALL);
+
+$executionStartTime = microtime(true);
+
+
+function geocoding($country){
+
+	$url='https://api.opencagedata.com/geocode/v1/json?q='. $country.'&key=d85b265402584acf90f094dd19ac6872';
+	$ch = curl_init();
+	curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false);
+	curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+	curl_setopt($ch, CURLOPT_URL,$url);
+	$result=curl_exec($ch);
+         //echo $result;
+	curl_close($ch);
+	$decode = json_decode($result,true);	
+	return $decode['results'][0]['geometry'] ;
+}
+
+$SelectLatLng = geocoding($_REQUEST['country']);
+
+//echo $SelectLatLng['lng'];
+//die($SelectLatLng['lat']);
+
 //echo $_REQUEST['lat'];
 //echo  $_REQUEST['lon'];
 
-	//$url='http://api.geonames.org/countryInfoJSON?formatted=true&lang=' . $_REQUEST['lang'] . '&country=' . $_REQUEST['country'] . '&username=saraapi';
     //key = fsq3TjgjfazQXKTJ7tXIzbWVtnkCsLqpc5lmEpXAC87r1IY=
     
     //$url='https://api.foursquare.com/v2/venues/search?ll='. $_REQUEST['lat'].','. $_REQUEST['lon'].'&categoryId=4bf58dd8d48988d196941735&client_id=EZQPVW3YPS2RFMN1DY4HFDPX2QJJQFJN3BUO003SD5TNF4A3&client_secret=E2E34IRKUGT3IOUHWPQMQVD4LE15R0XDIWHRA5POA4UHZOON&limit=1&v=20190425';
-    $url='https://api.foursquare.com/v2/venues/search?ll='. $_REQUEST['lat'].','. $_REQUEST['lon'].'&categoryId=4bf58dd8d48988d196941735&client_id=BOCHQZ4NF0D2NNFP2HM0INIKPUESPUX3RMRDUX02MPWIYSM2&client_secret=EDNX150PKLS4SMRHWL21Q0KLBAQXYQUQV5RAZI0HZSA1IYGG&limit=1&v=20190425';
-	//$url='https://api.foursquare.com/v2/venues/search?ll=51.6268068,-0.2716706&categoryId=4bf58dd8d48988d196941735%20&client_id=EZQPVW3YPS2RFMN1DY4HFDPX2QJJQFJN3BUO003SD5TNF4A3&client_secret=E2E34IRKUGT3IOUHWPQMQVD4LE15R0XDIWHRA5POA4UHZOON&limit=1&v=20180628';
+    $url='https://api.foursquare.com/v2/venues/search?ll='. $SelectLatLng['lat'].','. $SelectLatLng['lng'].'&categoryId=4bf58dd8d48988d196941735&client_id=BOCHQZ4NF0D2NNFP2HM0INIKPUESPUX3RMRDUX02MPWIYSM2&client_secret=EDNX150PKLS4SMRHWL21Q0KLBAQXYQUQV5RAZI0HZSA1IYGG&limit=50&v=20190425';
+	//$url='https://api.foursquare.com/v2/venues/search?ll='. $SelectLatLng['lat'].','. $SelectLatLng['lng'].'&categoryId=4bf58dd8d48988d196941735%20&client_id=EZQPVW3YPS2RFMN1DY4HFDPX2QJJQFJN3BUO003SD5TNF4A3&client_secret=E2E34IRKUGT3IOUHWPQMQVD4LE15R0XDIWHRA5POA4UHZOON&limit=1&v=20180323';
 
 // airport categorie = 4bf58dd8d48988d1ed931735
 
@@ -23,7 +44,7 @@
 	curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
 	curl_setopt($ch, CURLOPT_URL,$url);
 	$result=curl_exec($ch);
-//echo $result;
+  //echo $result;
 	curl_close($ch);
 	$decode = json_decode($result,true);	
 /*
@@ -37,7 +58,8 @@
 	$output['status']['name'] = "ok";
 	$output['status']['description'] = "success";
 	$output['status']['returnedIn'] = intval((microtime(true) - $executionStartTime) * 1000) . " ms";
-	$output['data'] = $decode['response']['venues'][0]['location'];
+	$output['data'] = $decode['response']['venues'];
+	
 	
 	header('Content-Type: application/json; charset=UTF-8');
 
